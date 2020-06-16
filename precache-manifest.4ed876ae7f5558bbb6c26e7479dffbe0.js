517b13875f2fcafab4d45159ec4623a17beb8cb6
David Moutray <dmoutray@live.co.uk>
2020-06-16 21:58:12 +0100

    host

---
+++ precache-manifest.4ed876ae7f5558bbb6c26e7479dffbe0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8f02f910c10b3f2eb0b815de13a370e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9d1678591c20701f9a9a",
+    "url": "/static/css/main.db39dd68.chunk.css"
+  },
+  {
+    "revision": "b68ba0a00cdb1faa1f5c",
+    "url": "/static/js/2.cca8fb7d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.cca8fb7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d1678591c20701f9a9a",
+    "url": "/static/js/main.8e44f89b.chunk.js"
+  },
+  {
+    "revision": "2935eb00d73d7c11a62d",
+    "url": "/static/js/runtime-main.28c6cd7a.js"
+  },
+  {
+    "revision": "45ff6a59996e3ba8ff795a372e9ff520",
+    "url": "/static/media/cross-icon.45ff6a59.png"
+  },
+  {
+    "revision": "2d0fe035ce145302f0c78d00ee14eb2a",
+    "url": "/static/media/tick-icon.2d0fe035.png"
+  }
+]);
